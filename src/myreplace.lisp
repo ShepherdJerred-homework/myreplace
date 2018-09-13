c480b54949bcc2e3474be68d118207fd37d8bab3
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-09-13 12:46:16 -0500

    Impl

--- src/myreplace.lisp
+++ src/myreplace.lisp
@@ -1,8 +1,27 @@
-(defun myreplace (old new target)
-  ()
-)
+; Jerred Shepherd
 
-(myreplace 'name 'joe '(name is my favorite name)) ; => (joe is my favorite joe)
-(myreplace 'name 'joe 'name)                       ; => joe
-(myreplace 'name 'joe nil                          ; => nil
+(defun myreplace (old new target)
+  (if (null target)  nil
+    (if (listp target)
+         (let ((curr (first target)))
+           (if (listp curr)
+             (append (list (myreplace old new curr)) (myreplace old new (cdr target)))
+           (if (eq curr old)
+             (append (list new) (myreplace old new (cdr target)))
+             (append (list curr) (myreplace old new (cdr target)))
+             )
+           )
+           )
+       (if (eq target old)
+         new
+         target 
+         )
+       )
+     )
+    )
+
+; (myreplace 'name 'joe '(name is my favorite name)) ; => (joe is my favorite joe)
+; (myreplace 'name 'joe 'name)                       ; => joe
+; (myreplace 'name 'joe nil)                         ; => nil
+; (myreplace 'name 'joe '(name (name is name) name)) ; => (joe (joe is a joe) joe
 
